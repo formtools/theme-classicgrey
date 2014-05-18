5f80315077e0dcb39a261ff18f4e1f6a981049c4
Ben Keen <ben.keen@gmail.com>
2014-05-17 23:26:41 -0700

    1.0.2

--- about/theme.php
+++ about/theme.php
@@ -9,5 +9,5 @@ $theme_author = "Encore Web Studios";
 $theme_author_email = "formtools@encorewebstudios.com";
 $theme_link = "http://themes.formtools.org/classicgrey";
 $theme_description = "A remodelling of the original grey-styled Form Tools 1.x theme.";
-$theme_version = "1.0.1";
+$theme_version = "1.0.2";
 $supports_ft_versions = "2.0.0";
